7231d288103e2a61b83ef09c8cef69f56086f3c2
Rahul Jadhav <rahuljadhav0031@gmail.com>
2025-09-26 17:00:01 +0530

    otp box

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { NgOtpInputModule } from 'ng-otp-input';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [NgOtpInputModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
 
+
+  otp: string = '';
+
+  otpConfig = {
+    length: 4,
+    allowNumbersOnly: true,
+    isPasswordInput: false,
+    disableAutoFocus: false,
+    inputStyles: {
+      width: '40px',
+      height: '40px',
+      margin: '0 5px',
+      fontSize: '20px',
+      borderRadius: '5px',
+      border: '1px solid #ced4da'
+    }
+  };
+
+  onOtpChange(otpValue: string) {
+    this.otp = otpValue;
+  }
+
+  clearOtp() {
+    this.otp = '';
+    console.log("OTP Cleared");
+  }
+
+  verifyOtp() {
+    alert("OTP Submitted: " + this.otp);
+  }
 }
